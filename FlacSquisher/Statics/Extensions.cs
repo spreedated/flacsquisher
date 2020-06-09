0e42bb0e2eb0b30e1af9a8bcaaa621673d06b098
SpReeD <wackermann@nexn.systems>
2020-06-09 16:09:51 +0200

    ~ Extension cleanup

--- FlacSquisher/Statics/Extensions.cs
+++ FlacSquisher/Statics/Extensions.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace FlacSquisher
 {
     public static class Extensions
     {
-        public static Task DoAll(this IEnumerable<Task> x)
-        {
-            Task t = new Task(() =>
-            {
-                x.All(y => { y.Start(); return true; });
-                Task.WaitAll(x.ToArray());
-            });
-            t.Start();
-            return t;
-        }
         public static string GetEnumDescription(this Enum value)
         {
             FieldInfo fi = value.GetType().GetField(value.ToString());
